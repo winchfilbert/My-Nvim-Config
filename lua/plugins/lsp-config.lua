4d511a9e7eb1ff5c961967bde4d9e62940eae7f9
FChang88 <fchristianwinch@gmail.com>
2025-02-12 12:07:33 +0700

    added some fixed to the dependency

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return{
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {"lua_ls", "clangd", "cssls", "dockerls", "gopls",
-          "html", "jsonls", "tsserver", "eslint", "ast_grep", "pylsp","rust_analyzer", "tailwindcss"}
+          "html", "jsonls", "ts_ls", "eslint", "ast_grep", "pylsp","rust_analyzer", "tailwindcss"}
       })
     end
   },
@@ -21,7 +21,7 @@ return{
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local servers = { "lua_ls", "clangd", "tsserver", "gopls", "html", "cssls", "ast_grep", "pylsp", "tailwindcss" }
+      local servers = { "lua_ls", "clangd", "ts_ls", "gopls", "html", "cssls", "ast_grep", "pylsp", "tailwindcss" }
       for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup({
           capabilities = capabilities
